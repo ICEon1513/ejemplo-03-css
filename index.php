394bfd491c22fd2fbcbad2f7e340576ea5b1f753
ICEon1513 <72657408+ICEon1513@users.noreply.github.com>
2020-10-11 07:46:31 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo css</title>
+</head>
+<body>
+    <div>
+        <h1>Bienvenido</h1>
+        <nav id="menuppal">
+            <ul>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
